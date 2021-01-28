448b3f5bca05452b91d7e204a27389f1951c66af
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2021-01-28 10:10:14 +0100

    Ajout case et grille

---
+++ Sudoku/Grille.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Grille
+    {
+        private List<Case> Cases = new List<Case>();
+
+        public Grille()
+        {
+            
+        }
+    }
+}
